f98415a5f77cd03e4bde833c9ea4533a0ec588ae
Kunal Bhalla <kunalb@fb.com>
2024-05-08 04:55:24 -0400

    Set up index card creation

--- main.go
+++ main.go
@@ -6,12 +6,16 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"time"
 )
 
 import flag "github.com/spf13/pflag"
 
 func redirectLogs() func() {
-	logFile, err := os.OpenFile("termdex.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	logFile, err := os.OpenFile("/tmp/termdex.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -78,8 +82,10 @@ func help() {
 	println("TermDex -- terminal index cards\n")
 	println("Subcommands: ")
 	println("  board: visualize existing cards")
-	println("  new: create a new card")
+	println("  browse: browse all notes")
 	println("  init: register a directory as a termdex")
+	println("  new: create a new card")
+	println("  lsp: run as an lsp for $EDITOR, useful for linking")
 	println()
 }
 
@@ -88,7 +94,47 @@ func Init() {
 }
 
 func NewCard() {
-	println("Making a new card!")
+	// TODO Figure out a reasonable template for this card
+	// Based on the other cards in the current dex folder
+	editorBinary := os.Getenv("EDITOR")
+	if editorBinary == "" {
+		log.Println(
+			"Couldn't find a configured editor: set the EDITOR environment" +
+				"variable to a suitable editor. Falling back to nano.")
+		editorBinary = "nano"
+	}
+
+	currentTime := time.Now()
+	indexCardDir := currentTime.Format("2006/01/02")
+
+	fileName := strconv.FormatInt(currentTime.Unix(), 10) + ".md"
+
+	dirErr := os.MkdirAll(indexCardDir, os.ModePerm)
+	if dirErr != nil {
+		log.Fatal(dirErr)
+	}
+
+	cardPath := filepath.Join(indexCardDir, fileName)
+
+	cmdPath, cmdPathErr := exec.LookPath(editorBinary)
+	if cmdPathErr != nil {
+		log.Panic(cmdPathErr)
+	}
+
+	cmd := exec.Command(cmdPath, cardPath)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = os.Environ()
+	editErr := cmd.Run()
+	if editErr != nil {
+		log.Fatal(editErr)
+	}
+
+	cmdErr := cmd.Wait()
+	if cmdErr != nil {
+		log.Fatal("Failed to run vim: ", cmdErr)
+	}
 }
 
 func Board() {
