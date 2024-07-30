7701e98cc2b02b16a685f6d7b044e63b6219134d
Kunal Bhalla <bhalla.kunal@gmail.com>
2024-07-30 12:03:37 -0400

    Remove arbitrary test files

--- markdown_files/sql.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eu
-
-pushd ~/dev/termdex/markdown_files/ >/dev/null
-zig build
-popd 2>/dev/null
-
-sqlite3 ":memory:" -column -cmd ".load ./zig-out/lib/libmarkdown_files.so sqlite3_markdown_files_init" "$@"
